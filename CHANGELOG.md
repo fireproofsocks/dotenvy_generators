0d3a3da6b955e5a53785ea7dce95b42d234d57ac
Everett Griffiths <everett@fireproofsocks.com>
2025-01-05 11:24:09 -0500

    Prep for hex publication

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## v0.9.0
+
+Initial release. Version number matches corresponding version of `Dotenvy`. Includes tasks:
+
+- `dot.new`
+- `phx.new.ecto`
+- `phx.new`
+- `phx.new.web`
--- mix.exs
+++ mix.exs
@@ -1,16 +1,25 @@
 defmodule DotenvyGenerators.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/fireproofsocks/dotenvy_generators"
+  @version "0.9.0"
+
   def project do
     [
       app: :dotenvy_generators,
-      version: "0.1.0",
+      version: @version,
       phoenix_version: "1.7.18",
       description:
         "Generators for Elixir apps using Dotenvy for config including variants of the phx_new generators",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: [
+        source_ref: "v#{@version}",
+        source_url: @source_url,
+        extras: extras()
+      ]
     ]
   end
 
@@ -24,5 +33,35 @@ defmodule DotenvyGenerators.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     []
+  end
+
+  def extras do
+    [
+      "CHANGELOG.md"
+    ]
+  end
+
+  def links do
+    %{
+      "GitHub" => @source_url,
+      "Readme" => "#{@source_url}/blob/v#{@version}/README.md",
+      "Changelog" => "#{@source_url}/blob/v#{@version}/CHANGELOG.md"
+    }
+  end
+
+  defp package do
+    [
+      maintainers: ["Everett Griffiths"],
+      licenses: ["Apache-2.0"],
+      logo: "assets/logo.png",
+      links: links(),
+      files: [
+        "lib",
+        "mix.exs",
+        "README*",
+        "CHANGELOG*",
+        "LICENSE*"
+      ]
+    ]
   end
 end
